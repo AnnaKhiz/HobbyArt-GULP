cc452ad38f31da294634df17200648b38f9c6a2f
AnnaKhiz <kukurika.anna@gmail.com>
2023-12-14 15:52:13 +0200

    add back button, edit position

--- src/js/modules/back.js
+++ src/js/modules/back.js
@@ -1,6 +1,7 @@
 export function backToPortfolio() {
   const backButton = document.getElementById('button-back');
   backButton.addEventListener('click', () => {
-    location.href = 'https://anna-khizhniak.site/'
+    location.replace('https://anna-khizhniak.site/')
+
   })
 }
